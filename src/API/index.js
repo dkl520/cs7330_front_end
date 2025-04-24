ed00f2bf423c5e80817d8a49dd4ca0e071f353b1
root <dkl520sss@gmail.com>
2025-04-24 00:25:36 -0500

    fixed bug

--- src/API/index.js
+++ src/API/index.js
@@ -5,23 +5,20 @@ import { repostApi } from './repostApi';
 import { instituteApi } from './instituteApi';
 import { projectApi } from './projectApi';
 import { projectFieldApi } from './projectFieldApi';
-// import { projectPostApi } from './projectPostApi';
 import { analysisResultApi } from './analysisResultApi';
 import { experimentApi } from './experimentApi';
-import { projectPostApi } from './projectPostApi';
-import analysisResultApi from './analysisResultApi';
+import { projectPost } from './projectPost';
 // 创建一个统一的API对象
 const api = {
   user: userApi,
   media: mediaApi,
   post: postApi,
-  projectPost: projectPostApi,
+  projectPost: projectPost,
   analysisResult: analysisResultApi,
   repost: repostApi,
   institute: instituteApi,
   project: projectApi,
   projectField: projectFieldApi,
-  //   projectPost: projectPostApi,
   analysisResult: analysisResultApi,
   experiment: experimentApi
 };
